942422d354b5e56596e7a2e072852e80d643c164
Yash Panchal <yp06446@gmail.com>
2024-05-11 21:44:27 +0530

    defined controllers folder and defined the authControllers

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,14 +1,10 @@
 import express from "express";
+import { postLogin, postRegister } from "../controllers/controllers.js";
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-    return res.send("This is register route");
-});
+router.post("/register", postRegister);
 
-router.post("/login", (req, res) => {
-    return res.send("This is login route");
-});
-
+router.post("/login", postLogin);
 
 export default router;
